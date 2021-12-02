9e6d5dbc2facf063b0e95d6d780d12a2f15344a9
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-12-02 16:44:12 -0500

    wokring onreact

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 import ContactCard from './components/ContactCard';
 
 function App() {
+  const arr1=[1,2,3,4];
+  const arr2=[2,3,4,5];
+  const arr3=[...arr1,...arr2]
+  console.log(arr3)
   
   return (
    <h1>hello world</h1>

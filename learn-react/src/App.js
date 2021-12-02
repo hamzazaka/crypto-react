8292674baccca288f03be9d1d67650fe3cc8b704
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-12-02 16:40:20 -0500

    working on react

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -6,11 +6,7 @@ import ContactCard from './components/ContactCard';
 function App() {
   
   return (
-    <div className="">
-        <ContactCard name='hamza' email='hamza@gmail.com' age='22'/>
-        <ContactCard name='john' email='john@gmail.com' age='42'/>
-        <ContactCard name='adrain' email='adrain@gmail.com' age='32'/>
-    </div>
+   <h1>hello world</h1>
   );
 }
 
